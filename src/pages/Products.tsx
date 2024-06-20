3bede0037cfafb0ea17a8edd71ec1249f971e1b9
NikDoe <nikd0e@yandex.ru>
2024-06-20 17:38:05 +0300

    setup products page components

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,23 @@
-function Products() {
+import { Filters, ProductsContainer, PaginationContainer } from '@/components';
+import { customFetch, type ProductsResponse } from '../utils';
+import { type LoaderFunction } from 'react-router-dom';
+
+const url = '/products';
+
+export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
+	const response = await customFetch<ProductsResponse>(url);
+
+	return { ...response.data };
+};
+
+const Products = () => {
 	return (
-		<div>
-            Products Page
-		</div>
+		<>
+			<Filters />
+			<ProductsContainer />
+			<PaginationContainer />
+		</>
 	);
-}
+};
 
 export default Products;
