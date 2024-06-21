e143c18c189cd5c8e691d4d6fc32dbf6529b1d26
NikDoe <nikd0e@yandex.ru>
2024-06-21 18:21:27 +0300

    add custom form input

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,8 +1,8 @@
 import { Form, Link, useLoaderData } from 'react-router-dom';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Button } from './ui/button';
 import { ProductsResponseWithParams } from '@/utils';
+
+import FormInput from './FormInput';
 
 function Filters() {
 	const { params } = useLoaderData() as ProductsResponseWithParams;
@@ -10,10 +10,13 @@ function Filters() {
     
 	return (
 		<Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-			<div className='mb-2'>
-				<Label htmlFor='search'>Search Product</Label>
-				<Input id='search' name='search' type='text' defaultValue={search} />
-			</div>
+			{/* search */}
+			<FormInput 
+				type='search'
+				label='search product'
+				name='search'
+				defaultValue={search} 
+			/>
 			<Button type='submit' size='sm' className='self-end mb-2'>
                 search
 			</Button>
