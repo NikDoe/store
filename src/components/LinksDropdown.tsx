2125ac5560bd87535e179c42ee6cbd1561b85a3f
NikDoe <nikd0e@yandex.ru>
2024-06-25 18:41:53 +0300

    access User in NavLinks, LinksDropdown and CartButton  Components

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -8,13 +8,18 @@ import { AlignLeft } from 'lucide-react';
 import { Button } from './ui/button';
 import { links } from '@/utils';
 import { NavLink } from 'react-router-dom';
+import { useAppSelector } from '@/hooks';
 
 function LinksDropdown() {
+	const user = useAppSelector(
+		(state) => state.userState.user
+	);
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild className='lg:hidden'>
 				<Button variant='outline' size='icon'>
-					<AlignLeft />
+					<AlignLeft/>
 					<span className='sr-only'>Toggle links</span>
 				</Button>
 			</DropdownMenuTrigger>
@@ -24,6 +29,10 @@ function LinksDropdown() {
 				sideOffset={25}
 			>
 				{links.map((link) => {
+					const restrictedRoutes = link.href === 'checkout' || link.href === 'orders';
+
+					if (restrictedRoutes && !user) return null;
+					
 					return (
 						<DropdownMenuItem key={link.label}>
 							<NavLink
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,31 +4,34 @@ import { useAppSelector } from '@/hooks';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
-	// temp
-	const user = null;
+	const user = useAppSelector(
+		state => state.userState.user
+	);
 
 	const numItemsInCart = useAppSelector(
 		(state) => state.cartState.numItemsInCart
 	);
 
 	if (numItemsInCart === 0) {
-		return <SectionTitle text='Empty cart ☹️' />;
+		return <SectionTitle text='Empty cart ☹️'/>;
 	}
 
 	return (
 		<>
-			<SectionTitle text='Shopping Cart' />
+			<SectionTitle text='Shopping Cart'/>
 			<div className='mt-8 grid gap-8  lg:grid-cols-12'>
 				<div className='lg:col-span-8'>
-					<CartItemsList />
+					<CartItemsList/>
 				</div>
 				<div className='lg:col-span-4 lg:pl-4'>
-					<CartTotals />
-					{
-						user
-							? (<ProceedToCheckoutButton text='Proceed to checkout' to='/checkout' />)
-							: (<ProceedToCheckoutButton text='Please Login' to='/login' />)
-					}
+					<CartTotals/>
+					<Button asChild className='mt-8 w-full'>
+						{
+							user
+								? <Link to='/checkout'>Proceed to checkout</Link>
+								: <Link to='/login'>Please Login</Link>
+						}
+					</Button>
 				</div>
 			</div>
 		</>
@@ -36,9 +39,3 @@ const Cart = () => {
 };
 
 export default Cart;
-
-const ProceedToCheckoutButton = ({ text, to }: {text:string, to: string}) => {
-	return <Button asChild className='mt-8 w-full'>
-		<Link to={to}>{text}</Link>
-	</Button>;
-};
