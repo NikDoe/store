52e0f814250b897cd77c52fbdf38422f32768b17
NikDoe <nikd0e@yandex.ru>
2024-06-28 17:14:53 +0300

    setup orders page w/ loader

--- src/components/index.ts
+++ src/components/index.ts
@@ -26,3 +26,5 @@ export { default as CartItemsList } from './CartItemsList';
 export { default as CartTotals } from './CartTotals';
 export { default as SubmitButton } from './SubmitButton';
 export { default as CheckoutForm } from './CheckoutForm';
+export { default as OrdersList } from './OrdersList';
+export { default as ComplexPaginationContainer } from './ComplexPaginationContainer';
